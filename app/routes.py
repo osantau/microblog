8e5f512cc26e2fa228db0cacd4928626d609823b
osantau <octavian.santau.os@gmail.com>
2021-03-09 20:15:54 +0200

    Add Database support.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -29,5 +29,6 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remembre_me={}'.format(form.username.data, form.remember_me.data))
         print('Login requested for user {}, remembre_me={}'.format(form.username.data, form.remember_me.data))
-        redirect("/index")
+        redirect(url_for('index'))
+        
     return render_template("login.html", title="Log In", form = form)

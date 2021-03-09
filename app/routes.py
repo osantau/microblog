6bf9f75064fbb51e1c69ed3ca17fa91a70bfcca8
Octavian Santau <octavian.santau@palstor.ro>
2021-03-09 15:09:08 +0200

    Add login page.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route("/")
 @app.route("/index")
@@ -21,3 +22,12 @@ def index():
 @app.route("/contact")
 def contact():
 	return render_template("contact.html", title="Contact")
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remembre_me={}'.format(form.username.data, form.remember_me.data))
+        print('Login requested for user {}, remembre_me={}'.format(form.username.data, form.remember_me.data))
+        redirect("/index")
+    return render_template("login.html", title="Log In", form = form)
